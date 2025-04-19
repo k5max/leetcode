ac96f443bd3443a94e8dd224b0f90c7dfc65f121
k5max <aledsanlw@gmail.com>
2025-04-20 00:33:10 +0800

    feat: 143_重排链表

---
+++ java/0143_reorder-list.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode mid = getMidNode(head);
+        ListNode head2 = reverseList(mid);
+        while (head2.next != null) {
+            ListNode nxt1 = head.next;
+            ListNode nxt2 = head2.next;
+            head.next = head2;
+            head2.next = nxt1;
+            head = nxt1;
+            head2 = nxt2;
+        }
+    }
+
+    public ListNode getMidNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+}
