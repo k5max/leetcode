24220762e54bed8d18350ddbad364cd823c13102
k5max <aledsanlw@gmail.com>
2025-04-20 22:42:42 +0800

    feat: 704_二分查找

---
+++ java/数组/0704_binary-search.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = left + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
