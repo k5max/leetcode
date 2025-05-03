4cdc6c6dbb500c5752df368d52671a6230d3fb9e
k5max <aledsanlw@gmail.com>
2025-05-03 23:46:07 +0800

    feat: 543_二叉树的直径

---
+++ java/二叉树/0543_diameter-of-binary-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int ans;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int l = depth(root.left);
+        int r = depth(root.right);
+        ans = Math.max(ans, l + r + 1);
+        return Math.max(l, r) + 1;
+    }
+}
