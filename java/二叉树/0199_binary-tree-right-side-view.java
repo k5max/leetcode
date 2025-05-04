1a4d41c1e05b33f878967d8bd235be8dc8d737a9
k5max <aledsanlw@gmail.com>
2025-05-04 23:24:01 +0800

    feat: 199_二叉树的右视图

---
+++ java/二叉树/0199_binary-tree-right-side-view.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<List<Integer>> levelList = levelOrder(root);
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < levelList.size(); i++) {
+            List<Integer> list = levelList.get(i);
+            ret.add(list.get(list.size() - 1)); 
+        }
+        return ret;
+    }
+
+    private List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int curLevelSize = queue.size();
+            for (int i = 0; i < curLevelSize; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
