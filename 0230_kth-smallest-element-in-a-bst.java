6ea4685c14250d256a8fd382dcfeefcecfb60c18
k5max <aledsanlw@gmail.com>
2025-05-04 21:52:24 +0800

    feat: 230_二叉搜索树中第 K 小的元素

---
+++ 0230_kth-smallest-element-in-a-bst.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int res, k;
+    public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        // 剪枝
+        if (k == 0)  return;
+        k = k - 1;
+        if (k == 0) res = root.val;
+        dfs(root.right);
+    }
+}
